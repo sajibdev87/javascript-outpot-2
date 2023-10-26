1533499ad3838a6bad88f6e9efd766f2450e6001
sajibdev87 <145119503+sajibdev87@users.noreply.github.com>
2023-10-25 21:44:36 -0700

    y

---
+++ sajib.js
@@ -0,0 +1,83 @@
+var numver = [45,65,76,45,456,87,];
+var age = [13,14,18,16,20,23,45,76,];
+var name = [sajib,roponkor,mazba,almin,chondhon,hridhy];
+console.log(numver);
+
+
+var numver = [45,65,76,45,456,87,];
+var age = [13,14,18,16,20,23,45,76,];
+console.log(age.length);
+
+
+var numver = [45,65,76,45,456,87,];
+var age = [13,14,18,16,20,23,45,76,];
+var index = numver.indexof(14);
+console.log(index);
+
+
+var age = [13,14,18,16,20,23,45,76,];
+var student = ['sajib' ,'rakib'];
+age[2] = 223;
+console.log(age);
+
+
+var student = ['sajib','msmun','roponkor'];
+student.push('rakib');
+console.log(student);
+
+
+var student = ['sajib','msmun','roponkor'];
+student.push('msmun');
+student.pop();
+console.log(student);
+
+
+var sajibLove = 100;
+var herLove = 99;
+if(sajibLove > herLove); {
+    console.log('sajib jitha geca ');
+}
+
+var hasFood = true;
+if (hasFood == true) {
+    console.log('khabar daw');
+}else{
+    console.log('khabar na thakle dio na ');
+}
+
+var smallBather = 12;
+if(smallBather < 14){
+    console.log('gortha niya jabo');
+}else{
+    console.log('niya jabo na');
+}
+
+
+var hasFlat = true;
+var govmentGob = false;
+var money = 100000;
+if(hasFlat == true || govmentGob == true && money > 100000){
+    console.log('yes');
+}else{
+   console.log('tor kopale beya nai re patgla');
+}
+
+
+var numver = 44;
+var orenge = 45;
+if(numver >orenge){
+    console.log('orenge khabo');
+    if(numver > 12){
+        console.log('nasta korbo')
+    }     
+}else{
+    console.log('khabo na');
+}
+
+
+
+
+
+
+
+
